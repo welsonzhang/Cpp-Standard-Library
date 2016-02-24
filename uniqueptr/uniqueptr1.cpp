6985c6c78abfe02a1e7481612457bd9eddafaa85
root <root@localhost.localdomain>
2016-02-24 13:56:23 +0800

    update

---
+++ uniqueptr/uniqueptr1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <memory> //for unique_ptr
+#include <dirent.h> //for opendir(),...
+#include <cstring> //for strerror()
+#include <cerrno> //for errno
+
+using namespace std;
+
+class DirCloser
+{
+  public:
+    void operator () (DIR* dp)
+    {
+      if(closedir(dp)!=0)
+      {
+         std::cerr<<"OOPS:closedir() failed"<<std::endl;
+      }
+    }
+};
+
+int main()
+{
+  //open current directory;
+  unique_ptr<DIR,DirCloser> pDir(opendir("."));
+
+  //process each directory entry;
+  struct dirent *dp;
+  while((dp=readdir(pDir.get()))!=nullptr)
+  {
+    string filename(dp->d_name);
+    cout<<"process "<<filename<<endl;
+  }
+}
